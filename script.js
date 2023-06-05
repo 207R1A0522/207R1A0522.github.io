380c4b189e124a44589b15dedbf1061db8bb4bb9
Suryabhaskar <118542487+207R1A0522@users.noreply.github.com>
2023-06-05 21:40:48 +0530

    performed first DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Suryabhaskar's page");
+var data1={image:"ABC.png",name: "Suryabhaskar", designation: "Student"};
+var data2={image:"DEF.png",name: "Kamma Suryabhaskar", designation: "Intern"};
+var isdata1=true;
+var display_data;
+var change_my_pic=function(){
+    if(isdata1)
+    {
+        display_data=data2;
+        isdata1=false;
+    }
+    else
+    {
+        display_data=data1;
+        isdata1=true;
+    }
+    document.getElementById("my_image").src=display_data.image;
+}
