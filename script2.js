c871e7deb108faf3e82794fc42dcc48dd3638363
Suryabhaskar <118542487+207R1A0522@users.noreply.github.com>
2023-06-06 21:45:31 +0530

    added script.js file

---
+++ script2.js
@@ -0,0 +1,11 @@
+var user_info={name:"",image:"",username:""};
+var change_my_pic=function(){
+    fetch("https://randomuser.me/api").then((response)=>response.json()).then((data)=>{
+        user_info.name=data.results[0].name.first;
+        user_info.image=data.results[0].picture.large;
+        user_info.username=data.results[0].login.username;
+        document.getElementById("my_image").src=user_info.image;
+        document.getElementById("my_name").innerHTML=user_info.name;
+        document.getElementById("my_designation").innerHTML=user_info.username;
+    });
+}
